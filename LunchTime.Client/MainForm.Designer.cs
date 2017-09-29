a06b9ccd66c0d29d94534d924e0b94134c339c99
pdoerr <peter.doerr@reichhart.eu>
2017-09-29 13:33:13 +0200

    asdadasd

---
+++ LunchTime.Client/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace LunchTime.Client
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.RichTextBox_ChatMessages = new System.Windows.Forms.RichTextBox();
+            this.TextBox_ChatMessage = new System.Windows.Forms.TextBox();
+            this.Button_SendChatMessage = new System.Windows.Forms.Button();
+            this.ComboBox_Locations = new System.Windows.Forms.ComboBox();
+            this.Button_VoteForSelectedLocation = new System.Windows.Forms.Button();
+            this.Chart_LocationVotes = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.Chart_LocationVotes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // RichTextBox_ChatMessages
+            // 
+            this.RichTextBox_ChatMessages.Location = new System.Drawing.Point(12, 443);
+            this.RichTextBox_ChatMessages.Name = "RichTextBox_ChatMessages";
+            this.RichTextBox_ChatMessages.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.RichTextBox_ChatMessages.Size = new System.Drawing.Size(868, 188);
+            this.RichTextBox_ChatMessages.TabIndex = 0;
+            this.RichTextBox_ChatMessages.Text = "";
+            // 
+            // TextBox_ChatMessage
+            // 
+            this.TextBox_ChatMessage.Location = new System.Drawing.Point(12, 637);
+            this.TextBox_ChatMessage.Name = "TextBox_ChatMessage";
+            this.TextBox_ChatMessage.Size = new System.Drawing.Size(787, 20);
+            this.TextBox_ChatMessage.TabIndex = 1;
+            // 
+            // Button_SendChatMessage
+            // 
+            this.Button_SendChatMessage.Location = new System.Drawing.Point(805, 635);
+            this.Button_SendChatMessage.Name = "Button_SendChatMessage";
+            this.Button_SendChatMessage.Size = new System.Drawing.Size(75, 23);
+            this.Button_SendChatMessage.TabIndex = 2;
+            this.Button_SendChatMessage.Text = "Send";
+            this.Button_SendChatMessage.UseVisualStyleBackColor = true;
+            // 
+            // ComboBox_Locations
+            // 
+            this.ComboBox_Locations.FormattingEnabled = true;
+            this.ComboBox_Locations.Location = new System.Drawing.Point(680, 12);
+            this.ComboBox_Locations.Name = "ComboBox_Locations";
+            this.ComboBox_Locations.Size = new System.Drawing.Size(200, 21);
+            this.ComboBox_Locations.TabIndex = 3;
+            // 
+            // Button_VoteForSelectedLocation
+            // 
+            this.Button_VoteForSelectedLocation.Location = new System.Drawing.Point(805, 39);
+            this.Button_VoteForSelectedLocation.Name = "Button_VoteForSelectedLocation";
+            this.Button_VoteForSelectedLocation.Size = new System.Drawing.Size(75, 23);
+            this.Button_VoteForSelectedLocation.TabIndex = 4;
+            this.Button_VoteForSelectedLocation.Text = "Vote";
+            this.Button_VoteForSelectedLocation.UseVisualStyleBackColor = true;
+            // 
+            // Chart_LocationVotes
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.Chart_LocationVotes.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.Chart_LocationVotes.Legends.Add(legend1);
+            this.Chart_LocationVotes.Location = new System.Drawing.Point(12, 12);
+            this.Chart_LocationVotes.Name = "Chart_LocationVotes";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.Chart_LocationVotes.Series.Add(series1);
+            this.Chart_LocationVotes.Size = new System.Drawing.Size(662, 425);
+            this.Chart_LocationVotes.TabIndex = 5;
+            this.Chart_LocationVotes.Text = "chart1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 669);
+            this.Controls.Add(this.Chart_LocationVotes);
+            this.Controls.Add(this.Button_VoteForSelectedLocation);
+            this.Controls.Add(this.ComboBox_Locations);
+            this.Controls.Add(this.Button_SendChatMessage);
+            this.Controls.Add(this.TextBox_ChatMessage);
+            this.Controls.Add(this.RichTextBox_ChatMessages);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Chart_LocationVotes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextBox_ChatMessages;
+        private System.Windows.Forms.TextBox TextBox_ChatMessage;
+        private System.Windows.Forms.Button Button_SendChatMessage;
+        private System.Windows.Forms.ComboBox ComboBox_Locations;
+        private System.Windows.Forms.Button Button_VoteForSelectedLocation;
+        private System.Windows.Forms.DataVisualization.Charting.Chart Chart_LocationVotes;
+    }
+}
---
+++ LunchTime.Client/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+
+namespace LunchTime.Client
+{
+    /// <summary>
+    /// The Main-Form, used to visualize all Data.
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Initializes the Form.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// Loads the Form - called after the Constructor has been called.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            string[] locations = { "Burger", "Asia", "Tacco Tacco", "Dosenchili", "Burger1", "Asia1", "Tacco Tacco1", "Dosenchili1", "Burger2", "Asia2", "Tacco Tacco2", "Dosenchili2" };
+            int[] votes = { 1, 0, 4, 2, 1, 0, 4, 2, 1, 0, 4, 2 };
+
+            Chart_LocationVotes.Titles.Clear();
+            Chart_LocationVotes.Series.Clear();
+            Chart_LocationVotes.Titles.Add("Locations");
+
+            
+
+            for(int i = 0; i < locations.Length; i++)
+            {
+                Series series = Chart_LocationVotes.Series.Add(locations[i]);
+
+                series.Points.Add(votes[i]);
+            }
+        }
+    }
+}
